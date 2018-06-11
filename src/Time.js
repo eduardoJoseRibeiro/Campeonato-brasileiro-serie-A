1a25ce22dd2636e43c21f3438b3ff4a7fe9fa955
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2018-06-11 10:49:11 -0300

    Listagem dos times com estilo personalizado

---
+++ src/Time.js
@@ -0,0 +1,12 @@
+export default class Time {
+
+    constructor( nome, escudo ){
+
+        this.nome = nome;
+        this.escudo = escudo;
+
+        this.pontos = 0;
+        this.gm = 0;
+        this.gs = 0;
+    }
+}
