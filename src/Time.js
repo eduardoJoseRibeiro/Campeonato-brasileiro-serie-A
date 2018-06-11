ffc9bca352969b4897c875ceac36e132c982c43c
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2018-06-11 17:38:27 -0300

    Adicionado método de fim de jogo

--- src/Time.js
+++ src/Time.js
@@ -9,4 +9,29 @@ export default class Time {
         this.gm = 0;
         this.gs = 0;
     }
+
+    updateInfo(pontos, gols, golsSofridos){
+        
+        this.pontos += pontos;
+        this.gm += gols;
+        this.gs = golsSofridos; 
+    }
+
+    fimDeJogo(timeAdversario, golsMarcados, golsSofridos){
+
+        if( golsMarcados == golsSofridos ){
+
+            this.updateInfo( 1, golsMarcados, golsSofridos );
+            timeAdversario.updateInfo( 1, golsSofridos, golsMarcados );
+        }else if( golsMarcados > golsSofridos ){
+
+            this.updateInfo( 3, golsMarcados, golsSofridos );
+            timeAdversario.updateInfo( 0, golsSofridos, golsMarcados );
+        }else{
+
+            this.updateInfo( 0, golsMarcados, golsSofridos );
+            timeAdversario.updateInfo( 3, golsSofridos, golsMarcados );
+        }
+
+    }
 }
